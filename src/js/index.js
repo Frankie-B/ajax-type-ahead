e234a910051c435e78427c1afff0507ef997239f
Frankie Bukenya <frankie.bukenya@gmail.com>
2019-05-26 09:34:38 +0300

    Highlight the search term matches in the display

--- src/js/index.js
+++ src/js/index.js
@@ -14,7 +14,41 @@ fetch(endpoint)
 function findMatches(wordsToMatch, cities) {
   return cities.filter(place => {
     // We have to see fi the city or state matches what the user input
-    const regex = RegExp(wordsToMatch, 'gi');
+    const regex = new RegExp(wordsToMatch, 'gi');
     return place.city.match(regex) || place.state.match(regex);
   });
 }
+
+
+// Will display suggestions matching the users input
+function displayMatches() {
+  const matchArray = findMatches(this.value, cities);
+  // The code below hooks our data to the html document
+  const html = matchArray
+    .map(place => {
+      // To highlight the matched  search terms
+      const regex = new RegExp(this.value, 'gi');
+      const cityName = place.city.replace(
+        regex,
+        `<span class="hl">${this.value}</span>`
+      );
+      const stateName = place.state.replace(
+        regex,
+        `<span class="hl">${this.value}</span>`
+      );
+      return `
+    <li>
+        <span class="name">${cityName}, ${stateName}</span>
+        <span class="population">${place.population}</span>
+    </li>
+    `;
+    })
+    .join(''); // Will return our array item as a string
+  suggestions.innerHTML = html;
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
